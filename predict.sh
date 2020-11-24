18ef6a20ceeec3d02a19be2d1799d32dde004573
Deeachain <Deeachain@163.com>
2020-11-24 17:43:38 +0800

    rm project

--- predict.sh
+++
@@ -1,4 +0,0 @@
-python predict.py --dataset paris \
-                  --model DualSeg_res50 \
-                  --checkpoint /media/ding/Study/graduate/Segmentation_Torch/checkpoint/paris/DualSeg_res50bs4gpu1_train/model_20.pth \
-                  --tile_size 1024
